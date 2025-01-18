11f753735e59b60da51f3709d036a7e941be86bb
Mat <mat.frayne@gmail.com>
2025-01-18 19:54:39 +1000

    debug.

--- setup-script.js
+++ setup-script.js
@@ -90,7 +90,7 @@ const copyDir = () => {
 }
 
 const main = async () => {
-	console.log(process.cwd());
+	console.log(process.cwd(), import.meta.dirname);
 	const { projectName, template } = await getProjectName();
 	mkDir(projectName)
 	fs.cpSync("templates/" + template, projectName, { recursive: true });
